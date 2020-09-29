febd83e66111a9ffb44b2a572e7e9c421d81fc82
Jaeseong4156 <wotjd2679@gmail.com>
2020-09-29 13:09:36 +0900

    third commit

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jara:abcd1234@boilerplate.esjuc.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 새해복 마니바드세요, 라면 마니 드세요')
+})
+
+app.post('/register', (req, res) => {
+    
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것을 데이터 베이스에 넣는다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+
 })
 
 app.listen(port, () => {
